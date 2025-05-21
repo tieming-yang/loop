ee9743b7b091813f61521dfa331688a3b73faa26
Snow Yáng <ytm199891@gmail.com>
2025-05-21 14:22:22 +0900

    Add LoopPage component with metadata for title and description

---
+++ app/loop/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "環",
+  description: "科幻小說《環》",
+}
+
+export default async function LoopPage() {
+
+  return <p>Hi</p>
+}
+
+
