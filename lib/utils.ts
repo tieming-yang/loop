551267352441ed501b7ca5de3836c82e5272a806
Snow Yáng <ytm199891@gmail.com>
2025-05-21 17:16:28 +0900

    Add assertIsDefined utility function for value validation

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function assertIsDefined<T>(value: T, message: string): asserts value is NonNullable<T> {
+  if (value === undefined || value === null) {
+    throw new Error(message)
+  }
+}
