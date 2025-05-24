667b8d7b7bf5be5171886386744763d9348e2289
Snow Yáng <ytm199891@gmail.com>
2025-05-24 17:06:45 +0800

    Enhance LoopPage component styling by adding flex layout to the chapter list

--- app/loop/page.tsx
+++ app/loop/page.tsx
@@ -5,13 +5,12 @@ import Link from "next/link";
 
 export default async function LoopPage() {
   const manuScripts = await Loop.getAllManuScripts();
-
   if (!manuScripts || manuScripts.length === 0) {
     return <p>No chapters found</p>;
   }
 
   return (
-    <ul>
+    <ul className="flex flex-col gap-2 h-svh">
       {manuScripts.map((manuScript) => {
         const { id, title, chapter, createdTime } = manuScript;
 
