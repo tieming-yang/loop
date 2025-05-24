474bf63644ccdea863933b0e1ee20f35cda28763
Snow Yáng <ytm199891@gmail.com>
2025-05-24 17:06:50 +0800

    Refactor getManuScriptById to getManuScriptContentById and update implementation to use NotionConverter for HTML rendering

--- data/loop.ts
+++ data/loop.ts
@@ -1,6 +1,7 @@
 //@ts-nocheck
 import { assertIsDefined } from "@/lib/utils";
 import { Client, PageObjectResponse } from "@notionhq/client";
+import { NotionConverter } from "notion-to-md";
 
 // TODO: Move notion client to a separate file
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
@@ -49,14 +50,18 @@ const Loop = {
     }
   },
 
-  getManuScriptContentById: async (id: string) => {
+  getManuScriptById: async (id: string) => {
     assertIsDefined(id, "id is not defined");
     assertIsDefined(Loop.dbID, "Loop.dbID is not defined");
 
     try {
-      const page = await notion.pages.retrieve({ page_id: id });
-      console.log("Retrieved page:", page);
-      return page;
+      const n2m = new NotionConverter(notion);
+      const result = await n2m.convert(id, {
+        withRenderer: "html",
+      });
+
+      console.log("Retrieved page:", result);
+      return result;
     } catch (error) {
       console.error("Error fetching wiki entry:", error);
       throw new Error("Failed to fetch wiki entry");
