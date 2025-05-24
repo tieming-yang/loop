184f030a40d15660db3b5cddac9f8e9ad537debb
Snow Yáng <ytm199891@gmail.com>
2025-05-24 13:33:30 +0800

    Refactor LoopPage component to enhance chapter display with detailed information and fix link paths

--- app/loop/page.tsx
+++ app/loop/page.tsx
@@ -1,23 +1,33 @@
+//@ts-nocheck
 import Loop from "@/data/loop";
+import { PageObjectResponse } from "@notionhq/client";
 import Link from "next/link";
 
 export default async function LoopPage() {
-  const chapters = await Loop.getAllManuScripts()
+  const chapters = await Loop.getAllManuScripts();
   console.log("server chapters", chapters);
 
   if (!chapters || chapters.length === 0) {
     return <p>No chapters found</p>;
   }
 
-   return (
+  return (
     <ul>
-      {chapters.map((chap) => (
-        <li key={chap.id}>
-          <Link href={`/ring/${chap.id}`}>
-            {chap.title}
-          </Link>
-        </li>
-      ))}
+      {chapters.map((chap) => {
+        const page = chap as PageObjectResponse;
+        const id = page.id;
+        const title = page.properties.Title.title[0]?.plain_text;
+        const chapter = page.properties.Chapter.number;
+        const createdTime = Date(page.created_time).toLocaleString();
+
+        return (
+          <li key={page.id}>
+            <Link href={`/loop/${page.id}`}>
+              {chapter} - {title} - {createdTime}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
